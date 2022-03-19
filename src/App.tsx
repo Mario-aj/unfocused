9510a125fca0ecb453a045a468c700042a4eadc6
mario-aj <mariojorge1997@mail.ru>
2022-03-19 09:30:20 +0100

    chore: setup app with tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen justify-center items-center flex">
+      <h1 className="text-4xl text-red-600">Unfocused</h1>
     </div>
   );
 }
